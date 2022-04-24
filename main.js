5fb20bfd25ebb837eda0b570c4a4b888fc83ebdf
Zoe Ingram <zoeingram@Zoes-MacBook-Pro.local>
2022-04-24 11:22:33 -0700

    hiding/showing

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+let manifestoSpanishDisplay = false;
+let manifestoEnglishDisplay = false;
+
+
+
 $( "#manifestoButton" ).click(function() {
-    $( "#manifestoText" ).toggle(function() {
-    });
+    if (manifestoEnglishDisplay == true || manifestoSpanishDisplay == true) {
+        $( "#manifestoTextSpanish" ).hide(function() {
+            manifestoSpanishDisplay = false;
+        });
+        $( "#manifestoTextEnglish" ).hide(function() {
+            manifestoEnglishDisplay = false;
+        });
+    } else {
+        $( "#manifestoTextSpanish" ).show(function() {
+            manifestoSpanishDisplay = true;
+        });
+        $( "#manifestoTranslate" ).toggle(function() {
+        });
+    }
   });
 
-  mapVisual
-  $( "#mapButton" ).click(function() {
+$( "#mapButton" ).click(function() {
     $( "#mapVisual" ).toggle(function() {
     });
-  });
+});
+
+
+$( "#manifestoTranslate" ).click(function() {
+    $( "#manifestoTranslate" ).toggle(function() {
+    });
+    $( "#manifestoTextSpanish" ).hide(function() {
+        manifestoSpanishDisplay = false;
+    });
+    $( "#manifestoTextEnglish" ).show(function() {
+        manifestoEnglishDisplay = true;
+    });
+});
